151e321bbc80606ce37287d2d9a4f7010aade334
Glen Kirkup <glen.kirkup@flightdataservices.com>
2009-11-06 19:46:05 +0000

    I've forgotten what I changed, but it was for a good reason.

--- filterpype/data_filter.py
+++ filterpype/data_filter.py
@@ -2361,7 +2361,7 @@ class SetAttributesToData(dfb.DataFilter):
                     # Otherwise, treat it like a literal value
                     value = attr_val                    
             except AttributeError:
-                raise AttributeError, "There is no such attribute:", attribute
+                raise AttributeError, "There is no such attribute:" + attribute
             try:
                 value.append('')
                 value.pop()
