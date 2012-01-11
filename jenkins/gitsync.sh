d5e9abe15bc040b6c09e52fcbf3ba8ffe37013a9
Martin Wimpress <martin.wimpress@flightdataservices.com>
2012-01-11 15:25:48 +0000

    Updated .bzringore. Initial self hostng Jenkins scripts

---
+++ jenkins/gitsync.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Add Google authentication credentials
+if [ -e ~/.netrc ]; then
+    grep "machine code.google.com login flexiondotorg@gmail.com password fZ5PQ5cw4sC6" ~/.netrc || :
+
+    if [ $? -eq 1 ]; then
+        echo "machine code.google.com login flexiondotorg@gmail.com password fZ5PQ5cw4sC6" >> ~/.netrc
+    fi
+else
+    echo "machine code.google.com login flexiondotorg@gmail.com password fZ5PQ5cw4sC6" > ~/.netrc
+fi
+
+   
+mkdir -p /tmp/bzr2git/FilterPype || :
+cd /tmp/bzr2git/FilterPype
+
+# Get Bazaar code
+if [ ! -d FilterPype.bzr ]; then
+    bzr branch http://vindictive.flightdataservices.com/Bazaar/FilterPype/ FilterPype.bzr || :
+else
+    cd FilterPype.bzr
+    bzr pull
+    cd ..
+fi
+
+# Get Git code
+if [ ! -d FilterPype.git ]; then
+    git clone https://code.google.com/p/filterpype/ FilterPype.git || :
+else
+    cd FilterPype.git
+    git pull
+    cd ..
+fi
+    
+cd /tmp/bzr2git/FilterPype/FilterPype.git
+bzr fast-export --export-marks /tmp/bzr2git/FilterPype/FilterPype.marks /tmp/bzr2git/FilterPype/FilterPype.bzr | git fast-import || :
+git push || :
