256ca7df2c88a5427ce2c84830948db2427bbfbb
Martin Wimpress <martin.wimpress@flightdataservices.com>
2012-01-24 14:57:15 +0000

    Updated Jenkins scripts

--- jenkins/publish_pypi.sh
+++ jenkins/publish_pypi.sh
@@ -8,15 +8,24 @@ cd ${WORKSPACE}
 TAG_BUILD=`grep tag_build ${WORKSPACE}/setup.cfg | cut -d'=' -f2 | sed 's/ //g'`
 
 # If the build is tagged, in any way, it will not be published on PyPI
-if [ -n "${TAG_BUILD}" ]; then
-    # Publish to PyPI
-    echo "[distutils]"                 >  ~/.pypirc
-    echo "index-servers ="             >> ~/.pypirc
-    echo "    pypi"                    >> ~/.pypirc
-    echo ""                            >> ~/.pypirc
-    echo "[pypi]"                      >> ~/.pypirc
-    echo "username:flightdataservices" >> ~/.pypirc
-    echo "password:open1eye"           >> ~/.pypirc
-    python setup.py sdist upload
-    python setup.py upload_sphinx
+if [ -z "${TAG_BUILD}" ]; then
+    if [ -e ~/.pypirc ]; then
+        # Publish to PyPI
+        python setup.py sdist upload
+        python setup.py upload_sphinx
+    else
+        echo "This build is a release, but there is no '~/.pypirc' which"
+        echo "should look something like this"
+        echo
+        echo "[distutils]"
+        echo "index-servers ="
+        echo "    pypi"
+        echo
+        echo "[pypi]"
+        echo "username:yourusername"
+        echo "password:yourpssword"
+    fi
+else
+    echo "This is build is tagged : ${TAG_BUILD}"
+    echo "Therefore it will NOT be published on PyPI"
 fi
