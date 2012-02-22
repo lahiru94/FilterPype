e87dd14d3803ebe93e73a0057023513e5ae55571
Nick Pope <nick.pope@flightdataservices.com>
2012-02-22 18:27:46 +0000

    Added new requirements parser :)

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import re
-
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -34,73 +32,46 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-# http://cburgmer.posterous.com/pip-requirementstxt-and-setuppy
-def parse_requirements(file_name):
-    """
-    Extract all dependency names from requirements.txt.
-    """
-    requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line):
-            continue
-        if re.match(r'\s*-e\s+', line):
-            # TODO support version numbers
-            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
-        elif re.match(r'\s*-f\s+', line):
-            pass
-        else:
-            requirements.append(line)
-
-    requirements.reverse()
-    return requirements
-
-def parse_dependency_links(file_name):
-    """
-    Extract all URLs for packages not found on PyPI.
-    """
-    dependency_links = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'\s*-[ef]\s+', line):
-            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
-
-    dependency_links.reverse()
-    return dependency_links
- 
 from filterpype import __version__ as VERSION
 
-setup(   
+from requirements import RequirementsParser
+
+requirements = RequirementsParser()
+
+setup(
     name='FilterPype',
-    version = VERSION,
+    version=VERSION,
     url='http://www.filterpype.org/',
     author='Flight Data Services Ltd',
-    author_email='developers@flightdataservices.com',            
+    author_email='developers@flightdataservices.com',
     description='FilterPype is a process-flow pipes-and-filters Python framework.',
-    long_description = open('README').read() + open('CHANGES').read(),
+    long_description=open('README').read() + open('CHANGES').read(),
     download_url='http://www.filterpype.org/',
     license='MIT',
 
-    packages = find_packages(exclude=['lextab.*', 'parsetab.*']),                      
-    include_package_data = True,
+    packages=find_packages(exclude=['lextab.*', 'parsetab.*']),
+    include_package_data=True,
 
-    # Parse the 'requirements.txt' file to determine the dependencies.
-    install_requires = parse_requirements('requirements.txt'), 
-    dependency_links = parse_dependency_links('requirements.txt'),
-    setup_requires = ['nose>=1.0'],
-    test_suite = 'nose.collector',
-    
-    zip_safe = False,  
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",  	
+    install_requires=requirements.install_requires,
+    tests_require=requirements.tests_require,
+    extras_require=requirements.extras_require,
+    dependency_links=requirements.dependency_links,
+    setup_requires=['nose>=1.0'],
+    test_suite='nose.collector',
+
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.5",                
-        "Programming Language :: Python :: 2.6",        
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    	],                  
-    )
+    ],
+)
