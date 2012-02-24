b1ed34fc3a557fd919edc6ea4336df596079084b
Martin Wimpress <martin.wimpress@flightdataservices.com>
2012-02-24 16:07:01 +0000

    Added new requirements files and Jenkins scripts

--- jenkins/publish_googlecode.sh
+++ jenkins/publish_googlecode.sh
@@ -1,19 +1,75 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
+
+function usage() {
+    local MODE=${1}
+    echo "Usage"
+    echo
+    echo "  ${0} -v python_version"
+    echo
+    echo "Required parameters"
+    echo "  -u google_username  : Your googlecode account username"
+    echo "  -p google_password  : Your googlecode account password"    
+    echo "  -v python version   : The virtualenv Python version to use."
+    echo "Optional parameters"
+    echo "  -h                : This help"
+    exit 1
+}
+
+# Make sure we are running from within Jenkins
+if [ -z "${WORKSPACE}" ]; then
+    echo "ERROR! This script is designed to run from within a Jenkins build."
+    exit1
+fi
+
+# Init the variables
+VIRTVER=""
+GC_USERNAME=""
+GC_PASSWORD=""
+
+# Parse the options
+OPTSTRING=hp:u:v:
+while getopts ${OPTSTRING} OPT
+do
+    case ${OPT} in
+        h) usage;;
+        p) GC_PASSWORD=${OPTARG};;
+        u) GC_USERNAME=${OPTARG};;        
+        v) VIRTVER=${OPTARG};;
+        *) usage;;
+    esac
+done
+shift "$(( $OPTIND - 1 ))"
+
+if [ -z "${GC_USERNAME}" ] || [ -z "${GC_PASSWORD}" ]; then
+    echo "ERROR! You must provide your PyPI username and password."
+    exit 1
+fi
+
+VIRTENV=${WORKSPACE}/.py${VIRTVER}
+
+# Check the virtualenv exists
+if [ ! -f ${VIRTENV}/bin/python${VIRTVER} ]; then
+    echo "ERROR! Couldn't find the virtualenv : ${VIRTENV}"
+    exit 1
+fi
+
 # Enter the virtualenv
-VIRTENV=${WORKSPACE}/.pyenv
 . ${VIRTENV}/bin/activate
+
+# Enter the Jenkins workspace
 cd ${WORKSPACE}
 
 # Add Google authentication credentials
 if [ -e ~/.netrc ]; then
-    grep "machine code.google.com login flexiondotorg@gmail.com password fZ5PQ5cw4sC6" ~/.netrc || :
+    grep "machine code.google.com login ${GC_USERNAME} password ${GC_PASSWORD}" ~/.netrc || :
 
     if [ $? -eq 1 ]; then
-        echo "machine code.google.com login flexiondotorg@gmail.com password fZ5PQ5cw4sC6" >> ~/.netrc
+        echo "machine code.google.com login ${GC_USERNAME} password ${GC_PASSWORD}" >> ~/.netrc
     fi
 else
-    echo "machine code.google.com login flexiondotorg@gmail.com password fZ5PQ5cw4sC6" > ~/.netrc
+    echo "machine code.google.com login ${GC_USERNAME} password ${GC_PASSWORD}" > ~/.netrc
 fi
 
 mkdir -p /tmp/bzr2git/FilterPype || :
--- jenkins/publish_pypi.sh
+++ jenkins/publish_pypi.sh
@@ -1,8 +1,62 @@
 #!/usr/bin/env bash
 
+function usage() {
+    local MODE=${1}
+    echo "Usage"
+    echo
+    echo "  ${0} -v python_version"
+    echo
+    echo "Required parameters"
+    echo "  -u pypi_username  : Your PyPI account username"
+    echo "  -p pypi_password  : Your PyPI account password"    
+    echo "  -v python version : The virtualenv Python version to use."
+    echo "Optional parameters"
+    echo "  -h                : This help"
+    exit 1
+}
+
+# Make sure we are running from within Jenkins
+if [ -z "${WORKSPACE}" ]; then
+    echo "ERROR! This script is designed to run from within a Jenkins build."
+    exit1
+fi
+
+# Init the variables
+VIRTVER=""
+PYPI_USERNAME=""
+PYPI_PASSWORD=""
+
+# Parse the options
+OPTSTRING=hp:u:v:
+while getopts ${OPTSTRING} OPT
+do
+    case ${OPT} in
+        h) usage;;
+        p) PYPI_PASSWORD=${OPTARG};;
+        u) PYPI_USERNAME=${OPTARG};;        
+        v) VIRTVER=${OPTARG};;
+        *) usage;;
+    esac
+done
+shift "$(( $OPTIND - 1 ))"
+
+if [ -z "${PYPI_USERNAME}" ] || [ -z "${PYPI_PASSWORD}" ]; then
+    echo "ERROR! You must provide your PyPI username and password."
+    exit 1
+fi
+
+VIRTENV=${WORKSPACE}/.py${VIRTVER}
+
+# Check the virtualenv exists
+if [ ! -f ${VIRTENV}/bin/python${VIRTVER} ]; then
+    echo "ERROR! Couldn't find the virtualenv : ${VIRTENV}"
+    exit 1
+fi
+
 # Enter the virtualenv
-VIRTENV=${WORKSPACE}/.pyenv
 . ${VIRTENV}/bin/activate
+
+# Enter the Jenkins workspace
 cd ${WORKSPACE}
 
 # Get the tag.
@@ -12,21 +66,27 @@ TAG_BUILD=`grep tag_build ${WORKSPACE}/setup.cfg | cut -d'=' -f2 | sed 's/ //g'`
 if [ -n "${TAG_BUILD}" ]; then
     echo "This build is tagged : ${TAG_BUILD}"
     echo "Therefore it will NOT be published on PyPI"
-else    
-    if [ -e ~/.pypirc ]; then
-        # Publish to PyPI
-        python setup.py sdist upload
-        python setup.py upload_sphinx
+else   
+    echo "[distutils]"                  >  ~/.pypirc
+    echo "index-servers ="              >> ~/.pypirc
+    echo "    pypi"                     >> ~/.pypirc
+    echo                                >> ~/.pypirc
+    echo "[pypi]"                       >> ~/.pypirc
+    echo "username:${PYPI_USERNAME}" >> ~/.pypirc
+    echo "password:${PYPI_PASSWORD}" >> ~/.pypirc
+ 
+    # Publish to PyPI
+    python setup.py sdist upload
+
+    # Upload the Sphinx documentation to PyPI
+    # First make sure setup.cfg is configured for Sphinx upload
+    UPLOAD_SPHINX=`grep upload_sphinx setup.cfg`    
+    if [ $? -eq 0 ]; then
+        # Make sure there is something to upload
+        if [ -f ${WORKSPACE}/doc/build/html/index.html ]; then        
+          python setup.py upload_sphinx
+        fi
     else
-        echo "This build is a release, but there is no '~/.pypirc' which"
-        echo "should look something like this"
-        echo
-        echo "[distutils]"
-        echo "index-servers ="
-        echo "    pypi"
-        echo
-        echo "[pypi]"
-        echo "username:yourusername"
-        echo "password:yourpssword"
-    fi
+        echo "This build is NOT configured to upload docs to PyPI"
+    fi        
 fi
